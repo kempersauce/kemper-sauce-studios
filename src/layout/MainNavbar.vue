ec3888f967063ba3df19fcd9e6641a07e8daf25a
mj0ln1r42 <baileybp4@gmail.com>
2019-06-09 18:25:02 -0400

    Remove Components dropdown from navbar

--- src/layout/MainNavbar.vue
+++ src/layout/MainNavbar.vue
@@ -9,14 +9,6 @@
                 </router-link>
         </template>
         <template slot="navbar-menu">
-            <drop-down tag="li" title="Components" icon="now-ui-icons design_app" class="nav-item">
-                <nav-link to="/components">
-                    <i class="now-ui-icons business_chart-pie-36"></i> Components
-                </nav-link>
-                <a href="https://demos.creative-tim.com/vue-now-ui-kit-pro/documentation" target="_blank" class="dropdown-item">
-                    <i class="now-ui-icons design_bullet-list-67"></i> Documentation
-                </a>
-            </drop-down>
             <drop-down tag="li" title="projects" icon="now-ui-icons files_paper" class="nav-item">
                 <nav-link class="dropdown-item" to="/sections#projects">
                     <i class="now-ui-icons shopping_box"></i> Arcade of Light
