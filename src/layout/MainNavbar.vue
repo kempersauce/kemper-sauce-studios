c0511ff7b5e1d1eb002e1f74da1301c664ef5ab4
mj0ln1r42 <baileybp4@gmail.com>
2019-06-09 18:19:32 -0400

    About Us button - is hot trash but works

--- src/layout/MainNavbar.vue
+++ src/layout/MainNavbar.vue
@@ -22,47 +22,12 @@
                     <i class="now-ui-icons shopping_box"></i> Arcade of Light
                 </nav-link>
             </drop-down>
-
-            <drop-down tag="li" title="Examples" icon="now-ui-icons design_image" class="nav-item">
-                <nav-link to="/about">
-                    <i class="now-ui-icons business_bulb-63"></i> About-us
-                </nav-link>
-                <nav-link to="/blog-post">
-                    <i class="now-ui-icons text_align-left"></i> Blog Post
-                </nav-link>
-                <nav-link class="dropdown-item" to="/blog-posts">
-                    <i class="now-ui-icons design_bullet-list-67"></i> Blog Posts
-                </nav-link>
-                <nav-link class="dropdown-item" to="/contact">
-                    <i class="now-ui-icons location_pin"></i> Contact Us
-                </nav-link>
-                <nav-link class="dropdown-item" to="/landing">
-                    <i class="now-ui-icons education_paper"></i> Landing Page
-                </nav-link>
-                <nav-link class="dropdown-item" to="/login">
-                    <i class="now-ui-icons users_circle-08"></i> Login Page
-                </nav-link>
-                <nav-link class="dropdown-item" to="/pricing">
-                    <i class="now-ui-icons business_money-coins"></i> Pricing
-                </nav-link>
-                <nav-link class="dropdown-item" to="/ecommerce">
-                    <i class="now-ui-icons shopping_shop"></i> Ecommerce Page
-                </nav-link>
-                <nav-link class="dropdown-item" to="/product">
-                    <i class="now-ui-icons shopping_bag-16"></i> Product Page
-                </nav-link>
-                <nav-link class="dropdown-item" to="/profile">
-                    <i class="now-ui-icons users_single-02"></i> Profile Page
-                </nav-link>
-                <nav-link class="dropdown-item" to="/signup">
-                    <i class="now-ui-icons tech_mobile"></i> Signup Page
-                </nav-link>
-            </drop-down>
             <li class="nav-item">
-                <a class="nav-link btn btn-primary"
-                   href="https://www.creative-tim.com/product/vue-now-ui-kit-pro" target="_blank">
-                    <p>Buy Now</p>
+				<nav-link to="/about">
+                <a class="nav-link">
+                    <p>About Us</p>
                 </a>
+				</nav-link>
             </li>
 
         </template>
