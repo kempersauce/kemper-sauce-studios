3a14abdc04a6defe408a1ba661417df57c6fd605
mj0ln1r42 <baileybp4@gmail.com>
2019-06-09 18:43:00 -0400

    Start work on About Us page

--- src/pages/example-pages/AboutUs.vue
+++ src/pages/example-pages/AboutUs.vue
@@ -2,7 +2,7 @@
     <div class="wrapper">
         <div class="page-header page-header-small">
             <parallax class="page-header-image"
-                 style="background-image: url('img/bg32.jpg')">
+                 style="background-image: url('img/ks/group-photo.jpg')">
             </parallax>
             <div class="content-center">
                 <div class="row">
@@ -19,7 +19,7 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-8 mr-auto ml-auto">
-                                <h2 class="title">Simpler. Smarter. Faster.</h2>
+                                <h2 class="title">Kemper Sauce Studios</h2>
                                 <h4 class="description">The CloudCheckr Cloud Management Platform (CMP) provides
                                     full visibility and control to reduce costs, improve cybersecurity posture, and
                                     automate critical tasks.</h4>
